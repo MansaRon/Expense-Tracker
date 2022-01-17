1ab5bb95f117e47955d90c0c47c06668c63716cb
thendo <kramashia101@gmail.com>
2022-01-17 21:48:51 +0200

    Added a new library(gh-pages --save-dev) that will allow me to host the application on github & configured the package.json file.

--- src/App.js
+++ src/App.js
@@ -3,36 +3,36 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const dummy_data = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-  {
-    id: "e5",
-    title: "New Laptop",
-    amount: 890,
-    date: new Date(2022, 8, 6)
-  }
+  // {
+  //   id: "e1",
+  //   title: "Toilet Paper",
+  //   amount: 94.12,
+  //   date: new Date(2020, 7, 14),
+  // },
+  // {
+  //   id: "e2",
+  //   title: "New TV",
+  //   amount: 799.49,
+  //   date: new Date(2021, 2, 12),
+  // },
+  // {
+  //   id: "e3",
+  //   title: "Car Insurance",
+  //   amount: 294.67,
+  //   date: new Date(2021, 2, 28),
+  // },
+  // {
+  //   id: "e4",
+  //   title: "New Desk (Wooden)",
+  //   amount: 450,
+  //   date: new Date(2021, 5, 12),
+  // },
+  // {
+  //   id: "e5",
+  //   title: "New Laptop",
+  //   amount: 890,
+  //   date: new Date(2022, 8, 6)
+  // }
 ];
 
 function App() {
